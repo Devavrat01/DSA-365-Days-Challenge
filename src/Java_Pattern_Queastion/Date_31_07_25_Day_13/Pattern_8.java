78bcac67712a3f692d1626d5e154b9b459143a75
Devavrat <goluu572@gmail.com>
2025-08-03 23:05:15 +0530

    Day-16

--- src/Java_Pattern_Queastion/Date_31_07_25_Day_13/Pattern_8.java
+++ src/Java_Pattern_Queastion/Date_31_07_25_Day_13/Pattern_8.java
@@ -1,4 +1,24 @@
 package Java_Pattern_Queastion.Date_31_07_25_Day_13;
 
+import java.util.Scanner;
+
 public class Pattern_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = sc.nextInt();
+        int i=1;
+
+        while (i<=num){
+            int j=1;
+            char jthColmun= (char)('A'+i-1);
+            while (j<=i){
+
+                System.out.print(jthColmun);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
--- src/Java_Pattern_Queastion/Date_31_07_25_Day_13/Pattern_9.java
+++ src/Java_Pattern_Queastion/Date_31_07_25_Day_13/Pattern_9.java
@@ -1,4 +1,26 @@
 package Java_Pattern_Queastion.Date_31_07_25_Day_13;
 
+import java.util.Scanner;
+
 public class Pattern_9 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = sc.nextInt();
+        int i=1;
+
+        while (i<=num){
+            int j=1;
+            char ch= (char)('A'+i-1);
+            while (j<=num){
+                char ithrow= (char)(ch+j-1);
+                System.out.print(ithrow);
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+    }
+
